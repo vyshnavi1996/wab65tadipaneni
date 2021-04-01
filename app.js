9d4de640f97402b9042eb3e8958a9bc73e6a07bb
vyshnavi1996 <vyshu.tadipaneni14@gmail.com>
2021-03-31 21:33:15 -0500

    added all the files

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var graffie = require('./routes/graffie');
 var stars = require('./routes/stars');
+var slot = require('./routes/slot');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/graffie', graffie);
 app.use('/stars', stars);
+app.use('/slot', slot);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
